ad7466b2871f3860efba2413f51ec82505b20544
Gabriel <gabriel.alves@smartstaff.com.br>
2024-08-20 20:19:27 -0300

    Creating another components and giving move functions for post component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,43 @@ import { Sidebar} from "./components/Sidebar"
 import './global.css'
 import styles from './App.module.css'
 
+// author: {avatara_url: "" name:"" , role: ""}
+//publishedAt: Date
+//content: string
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/gabriel-ads.png",
+      name: "Gabriel Alves",
+      role: "Software Engineer"
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      {type: 'link', content: 'jane.design/doctorcare'},
+    
+    ],
+    publishedAt: new Date('2024-05-03 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/gabriel-ads.png",
+      name: "Lucas Nunes",
+      role: "Cyber Security"
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      {type: 'link', content: 'jane.design/doctorcare'},
+    
+    ],
+    publishedAt: new Date('2024-05-03 20:00:00')
+  }
+]
+
 function App() {
 
   return (
@@ -14,14 +51,9 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-          author="Gabriel Alves" 
-          content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed fuga aliquid quasi consequatur magnam alias sit accusantium, suscipit voluptatibus libero deserunt, quidem nam amet nihil, dignissimos atque. Quas, consectetur amet."
-          />
-          <Post 
-            author="Lucas Nunes"
-            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed fuga aliquid quasi consequatur magnam alias sit accusantium, suscipit voluptatibus libero deserunt, quidem nam amet nihil, dignissimos atque. Quas, consectetur amet."
-          />
+          {posts.map(()=>{
+            
+          })}
         </main>
       </div>
     </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { PencilLine } from '@phosphor-icons/react'
 import styles from './Sidebar.module.css'
+import { Avatar } from './Avatar'
 
 export function Sidebar() {
   return (
@@ -7,7 +8,7 @@ export function Sidebar() {
       <img className={styles.cover} src="https://images.unsplash.com/photo-1526925539332-aa3b66e35444?q=50&w=500&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
       
       <div className={styles.profile}>
-        <img className={styles.avatar} src="https://github.com/gabriel-ads.png" />
+      <Avatar src="https://github.com/gabriel-ads.png"/> 
 
         <strong>Gabriel Alves</strong>
         <span>Web Developer</span>
